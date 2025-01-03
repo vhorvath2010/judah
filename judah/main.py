ff2f90f08632975e92c8270f28325da99329d74c
Vince <vhorvath2010@gmail.com>
2025-01-03 00:57:51 -0500

    feat: setup logging

--- judah/main.py
+++ judah/main.py
@@ -1,5 +1,12 @@
+import logging
 import os
 
 from judah.openai_connector import OpenAIConnector
 
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+logger.info("Connecting to OpenAI...")
 openai = OpenAIConnector(api_key=os.environ.get("OPENAI_API_KEY"))
+logger.info("Connected to OpenAI successfully!")
